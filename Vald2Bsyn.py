56f3cca7018ab5b211db182d842a64600ca3afef
EricDepagne <eric@depagne.org>
2016-08-19 23:03:58 +0200

    Initialisation

---
+++ Vald2Bsyn.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Python imports                                                                                                 
+                                                                                                                 
+# import datetime                                                                                                
+
+# Numpy imports
+import numpy as np
+
