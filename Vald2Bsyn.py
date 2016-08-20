8e7df1859807b6943dbedb1cdaa2f8a1ab2e0e1c
EricDepagne <eric@depagne.org>
2016-08-20 21:05:31 +0200

    Gestion des paramètres

--- Vald2Bsyn.py
+++ Vald2Bsyn.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Python imports
+import sys
 import argparse
 
 # import datetime
@@ -12,14 +13,52 @@ import argparse
 first_ionisation_potential = [13.60, 24.59,  5.39,  9.32,  8.30, 11.26, 14.53, 13.62, 17.40, 21.56,  5.14,  7.64,  5.99,  8.15, 10.48, 10.36, 12.97, 15.76, 4.34,  6.11,  6.54,  6.82,  6.74,  6.77,  7.44,  7.87,  7.86, 7.64,  7.73,  9.39,  6.00,  7.88,  9.81,  9.75, 11.81, 14.00, 4.18,  5.69,  6.38,  6.84,  6.88,  7.10,  7.28,  7.36,  7.46, 8.33,  7.57,  8.99,  5.79,  7.34,  8.64,  9.01, 10.45, 12.13, 3.89,  5.21,  5.58,  5.65,  5.42,  5.49,  5.55,  5.63,  5.68, 6.16,  5.85,  5.93,  6.02,  6.10,  6.18,  6.25,  5.43,  7.00, 7.88,  7.98,  7.87,  8.50,  9.10,  9.00,  9.22, 10.44,  6.11, 7.42,  7.29,  8.42,  9.30, 10.75,  4.00,  5.28,  6.90,  6.08, 9.99,  6.00]
 second_ionisation_potential = [0.00, 54.42, 75.64, 18.21, 25.15, 24.38, 29.60, 35.12, 35.00, 40.96, 47.29, 15.03, 18.83, 16.35, 19.72, 23.33, 23.81, 27.62, 31.63, 11.87, 12.80, 13.58, 14.65, 16.50, 15.64, 16.18, 17.06, 18.17, 20.29, 17.96, 20.51, 15.93, 18.63, 21.19, 21.60, 24.36, 27.50, 11.03, 12.24, 13.13, 14.32, 16.15, 15.26, 16.76, 18.07, 19.42, 21.49, 16.90, 18.87, 14.63, 16.53, 18.60, 19.13, 21.21, 25.10, 10.00, 11.06, 10.85, 10.55, 10.73, 10.90, 11.07, 11.25, 12.10, 11.52, 11.67, 11.80, 11.93, 12.05, 12.17, 13.90, 14.90, 16.20, 17.70, 16.60, 17.50, 20.00, 18.56, 20.50, 18.76, 20.43, 15.03, 16.68, 19.00, 20.00, 21.00, 22.00, 10.14, 12.10, 11.50, 99.99, 12.00]
 
+
 def vald2bsyn():
     pass
 
+
+def checkinput(arguments):
+    """
+    We verify that the three mandatory parameters have been provided.
+    If not, then we ask for the missing ones
+    """
+    argdict = vars(arguments)
+    print('1 args : {0}'.format(argdict))
+    for k, v in list(argdict.items()):
+        print('{0} : {1}'.format(k, v))
+        if v is None:
+            print('Missing parameters {0}'.format(k))
+            var = input("Please enter something: ")
+            argdict[v] = str(var)
+    print('args : {0}'.format(argdict))
+
+    print('parameters: {0}'.format(len(sys.argv)))
+    if len(sys.argv) != 7:
+        print('too few parameters: {0}'.format(len(sys.argv)))
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='aperturephot')
-    parser.add_argument('-v', '--valdfile')
-    parser.add_argument('-b', '--bsynfile')
-    parser.add_argument('-e', '--extract_type')
+    parser = argparse.ArgumentParser(prog='Vald2Bsyn')
+    requiredNamed = parser.add_argument_group('required named arguments')
+    requiredNamed.add_argument(
+            '-v',
+            '--valdfile',
+            help='Name of the file containing the list of lines using the LONG Vald format.',
+            required=True
+    )
+    requiredNamed.add_argument(
+            '-b',
+            '--bsynfile',
+            help='Name of the file where the line list using the BSyn format will be saved',
+            required=True
+    )
+    requiredNamed.add_argument(
+            '-e',
+            '--extract_type',
+            help='Stellar or all',
+            required=True
+    )
     arguments = parser.parse_args()
 
+    checkinput(arguments)
     vald2bsyn()
